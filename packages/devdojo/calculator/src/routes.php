5c0c94b68634a0ca587b221d151affc9cbec57c8
lorinszky.mark <Newlife321>
2018-10-12 11:23:52 +0200

    revert

--- packages/devdojo/calculator/src/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('calculator', function(){
-    echo 'Hello from the calculator package!';
-});
-
-Route::get('add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
-Route::get('subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
